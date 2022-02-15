5dab1a9dddffe6e467f76c462c01954c25dcb77e
Ram Michaeli <ram153486@gmail.com>
2022-02-15 17:39:58 +0200

    Added Firestore and cloud messages

--- app/src/main/java/com/example/android2project/activities/SignInActivity.java
+++ app/src/main/java/com/example/android2project/activities/SignInActivity.java
@@ -22,5 +22,24 @@ public class SignInActivity extends AppCompatActivity {
     private void setListeners() {
         binding.textCreateNewAccount.setOnClickListener(v ->
                 startActivity(new Intent(getApplicationContext(),SingUpActivity.class)));
+        //        binding.buttonSignIn.setOnClickListener(v-> addDataToFirestore());
+
     }
+
+
+//    private void addDataToFirestore() {
+//        FirebaseFirestore database = FirebaseFirestore.getInstance();
+//        HashMap<String, Object> data = new HashMap<>();
+//        data.put("first_name","RamGalYulia");
+//        data.put("last_name","YuliaGalRam");
+//        database.collection("users")
+//                .add(data)
+//                .addOnSuccessListener(documentReference -> {
+//                    Toast.makeText(getApplicationContext(), "Data Inserted", Toast.LENGTH_SHORT).show();
+//                })
+//                .addOnFailureListener(exception -> {
+//                    Toast.makeText(getApplicationContext(), exception.getMessage(), Toast.LENGTH_SHORT).show();
+//                });
+//
+//    }
 }
