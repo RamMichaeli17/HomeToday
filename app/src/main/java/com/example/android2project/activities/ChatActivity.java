258c7717c8f42de9e7757e1288ad1cd6fd70af89
Ram Michaeli <ram153486@gmail.com>
2022-02-16 11:46:28 +0200

    Added Layout to chat

---
+++ app/src/main/java/com/example/android2project/activities/ChatActivity.java
@@ -0,0 +1,16 @@
+package com.example.android2project.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.android2project.R;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
